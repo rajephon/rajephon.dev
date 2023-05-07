0f8080cbb25db23e59bb19de5237704767f6f33e
rajephon <rajephon@gmail.com>
2023-05-07 17:30:38 +0900

    update project

--- site.config.ts
+++ site.config.ts
@@ -1,5 +1,7 @@
-module.exports = {
-  // where it all starts -- the site's root Notion page (required)
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
   rootNotionPageId: '4a4f4b39f5684c1ebfa4569d15aaf282',
 
   // if you want to restrict pages to a single notion workspace (optional)
@@ -13,13 +15,14 @@ module.exports = {
 
   // open graph metadata (optional)
   description: 'Chanwoo Noh',
-  socialImageTitle: 'Chanwoo Noh',
-  socialImageSubtitle: 'Chanwoo Noh',
 
   // social usernames (optional)
   twitter: 'rajephon',
   github: 'rajephon',
   linkedin: 'chanwoo-noh',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -27,17 +30,13 @@ module.exports = {
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // image CDN host to proxy all image requests through (optional)
-  // NOTE: this requires you to set up an external image proxy
-  imageCDNHost: null,
-
-  // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: null,
-
   // whether or not to enable support for LQIP preview images (optional)
-  // NOTE: this requires you to set up Google Firebase and add the environment
-  // variables specified in .env.example
-  isPreviewImageSupportEnabled: false,
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
@@ -47,5 +46,20 @@ module.exports = {
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null
-}
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  navigationStyle: 'default'
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //   }
+  // ]
+})
